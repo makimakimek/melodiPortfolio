01b33400a604e6f9dd866d74a807640be91bf668
makimakimek <melodiezgi17@gmail.com>
2024-07-14 11:26:16 +0300

    react router dom downloaded, individual project pages added, skinmate project page done for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,196 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Link } from "react-router-dom";
+import projectImage from './assets/skinmate.png';
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <>
+            <div className = "page">
+                <div className = "navAndProjects">
+                    <div className = "navigationAndName">
+                        <div className = "name">
+                            Melodi Keskin
+                        </div>
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+                        {/*<div className = "mood">
+                            mood:
+                        </div>*/}
+
+                        <div className = "navigationTab">
+                            <div className = "nodes">
+                                NODES
+                            </div>
+
+                            <div className = "menu">
+                                <Link to = "/">
+                                    <div className = "menuItem">
+                                        index
+                                    </div>
+                                </Link>
+                                
+                                <div className = "menuItem">
+                                    <div className = "dropdownProjects">
+                                        <div className = "dropdownTitle">
+                                            projects +
+                                        </div>
+
+                                        <div className = "dropdownContent">
+                                            <Link to = "/Skinmate" className = "dropdownItem">SkinMate iOS Application</Link>
+                                            <Link to = "/Skinmate" className = "dropdownItem">Countdown Timer</Link>
+                                            <Link to = "/Skinmate" className = "dropdownItem">InnoPromoo</Link>
+                                            <Link to = "/Skinmate" className = "dropdownItem">Wordle</Link>
+                                            <Link to = "/Skinmate" className = "dropdownItem">Memory Game</Link>
+                                            <Link to = "/Skinmate" className = "dropdownItem">Etch-A-Sketch</Link>
+                                            <Link to = "/Skinmate" className = "dropdownItem">Shopping List</Link>
+                                            <Link to = "/Skinmate" className = "dropdownItem">Cat Adoption</Link>
+                                            <Link to = "/Skinmate" className = "dropdownItem">Dice Roller</Link>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                                <a href = "https://www.google.com" className = "link">
+                                    <div className = "menuItem">
+                                        about me
+                                    </div>
+                                </a>
+                            </div> 
+                        </div>
+                    </div>
+
+                    <div className = "projectCollection">
+                        <div className = "mainTitle">
+                            COLLECTION OF MY PROJECTS
+                        </div>
+
+                        <div className = "projectsFull">
+                            <div className = "projectsRow">
+                                <Link to = "/Skinmate">
+                                    <div className = "projectContainer">
+                                        <div className = "projectImage">
+                                            <img src = {projectImage} className = "subImage" alt = "an image of FloraPod project"/>
+                                        </div>
+
+                                        <div className = "projectTitle">
+                                            SkinMate iOS Application
+                                        </div>
+                                    </div>
+                                </Link>
+                                
+                                <Link to = "/Skinmate">
+                                    <div className = "projectContainer">
+                                        <div className = "projectImage">
+                                            <img src = {projectImage} className = "subImage" alt = "an image of FloraPod project"/>
+                                        </div>
+
+                                        <div className = "projectTitle">
+                                            Countdown Timer
+                                        </div>
+                                    </div>
+                                </Link>
+
+                                <Link to = "/Skinmate">
+                                    <div className = "projectContainer">
+                                        <div className = "projectImage">
+                                            <img src = {projectImage} className = "subImage" alt = "an image of FloraPod project"/>
+                                        </div>
+
+                                        <div className = "projectTitle">
+                                            InnoPromoo
+                                        </div>
+                                    </div>
+                                </Link>
+                            </div>
+
+                            <div className = "projectsRow">
+                                <Link to = "/Skinmate">
+                                    <div className = "projectContainer">
+                                        <div className = "projectImage">
+                                            <img src = {projectImage} className = "subImage" alt = "an image of FloraPod project"/>
+                                        </div>
+
+                                        <div className = "projectTitle">
+                                            Wordle
+                                        </div>
+                                    </div>
+                                </Link>
+
+                                <Link to = "/Skinmate">
+                                    <div className = "projectContainer">
+                                        <div className = "projectImage">
+                                            <img src = {projectImage} className = "subImage" alt = "an image of FloraPod project"/>
+                                        </div>
+
+                                        <div className = "projectTitle">
+                                            Memory Game
+                                        </div>
+                                    </div>
+                                </Link>
+                                
+                                <Link to = "/Skinmate">
+                                    <div className = "projectContainer">
+                                        <div className = "projectImage">
+                                            <img src = {projectImage} className = "subImage" alt = "an image of FloraPod project"/>
+                                        </div>
+
+                                        <div className = "projectTitle">
+                                            Etch-A-Sketch
+                                        </div>
+                                    </div>
+                                </Link>
+                            </div>
+
+                            <div className = "projectsRow">
+                                <Link to = "/Skinmate">
+                                    <div className = "projectContainer">
+                                        <div className = "projectImage">
+                                            <img src = {projectImage} className = "subImage" alt = "an image of FloraPod project"/>
+                                        </div>
+
+                                        <div className = "projectTitle">
+                                            Shopping List
+                                        </div>
+                                    </div>
+                                </Link>
+
+                                <Link to = "/Skinmate">
+                                    <div className = "projectContainer">
+                                        <div className = "projectImage">
+                                            <img src = {projectImage} className = "subImage" alt = "an image of FloraPod project"/>
+                                        </div>
+
+                                        <div className = "projectTitle">
+                                            Cat Adoption
+                                        </div>
+                                    </div>
+                                </Link>
+
+                                <Link to = "/Skinmate">
+                                    <div className = "projectContainer">
+                                        <div className = "projectImage">
+                                            <img src = {projectImage} className = "subImage" alt = "an image of FloraPod project"/>
+                                        </div>
+
+                                        <div className = "projectTitle">
+                                            Dice Roller
+                                        </div>
+                                    </div>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className = "linkContainer">
+                    <Link to = "/">
+                        <div className = "indexLink">index</div>
+                    </Link>
+                    <Link to = "/Skinmate">
+                        <div className = "nextLink">next</div>
+                    </Link>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default App
